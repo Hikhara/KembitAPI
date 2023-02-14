bad93fff0599dd6cd0c8f19a71e9965dedd5b398
Rick <rick@alphenaar.org>
2023-02-14 13:34:28 +0100

    onsuccessvol API key toevoegen

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,12 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-//using WebApplication1.Models;
+using Microsoft.OpenApi.Models;
 using WebApplication1.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddMvc();
 builder.Services.AddControllers();
 //builder.Services.AddDbContext<KasteelContext>(options => options.UseInMemoryDatabase("Kasteel"));
 builder.Services.AddDbContext<KasteelContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("WebApplication1Context")));
@@ -15,7 +15,56 @@ builder.Services.AddDbContext<KoningContext>(options => options.UseSqlServer(bui
 //builder.Services.AddScoped<DbContext, KasteelContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+
+
+
+builder.Services.AddSwaggerGen(c => {
+c.SwaggerDoc("v1", new OpenApiInfo { Title = "[anything]", Version = "v1" });
+    /*c.AddSecurityDefinition("[auth scheme: same name as defined for asp.net]", new OpenApiSecurityScheme
+    {
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Name = "X-API-KEY", //header with api key
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+    });*/
+    /*var key = new OpenApiSecurityScheme()
+    {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "ApiKey"
+        },
+        In = ParameterLocation.Header
+    };
+    var requirement = new OpenApiSecurityRequirement
+                    {
+                             { key, new List<string>() }
+                    };
+    c.AddSecurityRequirement(requirement);*/
+    c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme()
+    {
+        Name = "x-api-key",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Description = "Authorization by x-api-key inside request's header",
+        Scheme = "ApiKeyScheme"
+    });
+
+    var key = new OpenApiSecurityScheme()
+    {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "ApiKey"
+        },
+        In = ParameterLocation.Header
+    };
+    var requirement = new OpenApiSecurityRequirement
+    {
+        { key, new List<string>() }
+    };
+    c.AddSecurityRequirement(requirement);
+});
 
 
 var app = builder.Build();
